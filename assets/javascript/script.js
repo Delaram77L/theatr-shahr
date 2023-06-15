9cf134e7da9574744b5b02ff0721c4524f49bf91
Lavaei <delaramlavaeei1377@gmail.com>
2023-06-15 21:46:44 +0330

    Internet connection with js

---
+++ assets/javascript/script.js
@@ -0,0 +1,21 @@
+
+// online & offline
+let el = document.createElement('div');
+document.getElementById("logotitr").prepend(el);
+
+
+window.addEventListener("offline", (event) => {
+    el.textContent = "اینترنت شما قطع شد";
+    el.className = "alert";
+    el.classList.add("alert-danger");
+    el.style.backgroundColor = '#f7bdb8';
+  });
+
+  window.addEventListener("online", (event) => {
+    el.textContent = "اینترنت شما وصل شد";
+    el.className = "alert";
+    el.classList.add("alert-success");
+    el.style.backgroundColor = '#b2f6d2';
+  });           
+
+
