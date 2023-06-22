6c204f73594248caa7a8e54a9e5c733f48a0c032
Lavaei <delaramlavaeei1377@gmail.com>
2023-06-23 00:03:12 +0330

    modify online & offline

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -4,28 +4,36 @@ let el = document.createElement('div');
 document.getElementById("logotitr").append(el);
 
 window.addEventListener("offline", (event) => {
+  el.style.display = 'block';
   el.textContent = "اینترنت شما قطع شد";
   el.className = "alert";
   el.classList.add("alert-danger");
   el.style.backgroundColor = '#f7bdb8';
+  setTimeout(() => {
+    el.style.display = 'none';
+  }, 3000);
 });
 
 window.addEventListener("online", (event) => {
+  el.style.display = 'block';
   el.textContent = "اینترنت شما وصل شد";
   el.className = "alert";
   el.classList.add("alert-success");
   el.style.backgroundColor = '#b2f6d2';
+  setTimeout(() => {
+    el.style.display = 'none';
+  }, 3000);
 });
 
 
 
 //accordion
-let accordion1 = document.getElementsByClassName('accordion1');
+// let accordion1 = document.getElementsByClassName('accordion1');
 
-Array.from(accordion1.children).forEach(wrapper => {
-  wrapper.getElementsByTagName('span').addEventListener('click', e => {
-    let span = e.target;
-    let wrapper = span.parentElement;
-    wrapper.classList.toggle('show');
-  })
-})
+// Array.from(accordion1.children).forEach(wrapper => {
+//   wrapper.getElementsByTagName('span').addEventListener('click', e => {
+//     let span = e.target;
+//     let wrapper = span.parentElement;
+//     wrapper.classList.toggle('show');
+//   })
+// })
