4fcd3bc8fe7016705597bfcbd0fc31286952e722
Lavaei <delaramlavaeei1377@gmail.com>
2023-06-22 21:51:27 +0330

    Creating an incomplete accordion

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -3,19 +3,29 @@
 let el = document.createElement('div');
 document.getElementById("logotitr").append(el);
 
-
 window.addEventListener("offline", (event) => {
-    el.textContent = "اینترنت شما قطع شد";
-    el.className = "alert";
-    el.classList.add("alert-danger");
-    el.style.backgroundColor = '#f7bdb8';
-  });
+  el.textContent = "اینترنت شما قطع شد";
+  el.className = "alert";
+  el.classList.add("alert-danger");
+  el.style.backgroundColor = '#f7bdb8';
+});
 
 window.addEventListener("online", (event) => {
   el.textContent = "اینترنت شما وصل شد";
   el.className = "alert";
   el.classList.add("alert-success");
   el.style.backgroundColor = '#b2f6d2';
-  });           
+});
 
 
+
+//accordion
+let accordion1 = document.getElementsByClassName('accordion1');
+
+Array.from(accordion1.children).forEach(wrapper => {
+  wrapper.getElementsByTagName('span').addEventListener('click', e => {
+    let span = e.target;
+    let wrapper = span.parentElement;
+    wrapper.classList.toggle('show');
+  })
+})
